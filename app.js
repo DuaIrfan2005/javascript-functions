e3f7f2ef043c8a5d0277ded8fdfd8194911ae4f4
DuaIrfan2005 <155565372+DuaIrfan2005@users.noreply.github.com>
2024-05-02 16:40:20 +0500

    javascript functions

---
+++ app.js
@@ -0,0 +1,5 @@
+alert ("Error! please enter a valid password");
+alert ("Welcome to Js Land...\n Happy Coding!");
+alert ("Welcome to Js Land");
+alert ("Happy Coding!\n Prevent this page from creating additional dialogs ");
+alert ("Hello... I can run JS through my web browser's console");
